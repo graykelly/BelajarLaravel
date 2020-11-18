82de0748ad413282481e0b003bffa9fc65b2c0fe
Kelly <graykelly97@gmail.com>
2020-11-18 20:47:28 +0700

    Membuat navigation

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('title', 'About')
+@section('head')
+    <style>
+        body {
+            background-color: gray;
+        }
+    </style>
+@endsection
+@section('content')
+<div class="container">
+    About
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Belajar Laravel</title>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    @yield('head')
 </head>
 <body>
-    
+    @include('layouts.nav')
+    <div class="py-4">
+        @yield('content')
+    </div>
 </body>
 </html>
